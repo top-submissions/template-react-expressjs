a0f1bdb565455f580155e948c7040ba26d0dd5ef
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-02-11 19:49:19 +0800

    feat(error): add errors/, CustomNotFoundError.js
    Why:
    - Error callback used in app.js can only send error code 500

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use('/', appRouter);
 app.use((err, req, res, next) => {
   console.log(err);
-  res.status(500).send(err);
+  res.status(err.statusCode || 500).send(err.message);
 });
 
 const PORT = process.env.PORT || 3000;
---
+++ server/errors/CustomNotFoundError.js
@@ -0,0 +1,7 @@
+export default class CustomNotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = 404;
+    this.name = 'NotFoundError';
+  }
+}
