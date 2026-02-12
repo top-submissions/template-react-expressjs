15429f91b3802a71c9483a0ac32da8329e10ccee
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-02-12 21:11:59 +0800

    feat(router): pass links when rendering index in appRouter

--- server/routes/appRouter.js
+++ server/routes/appRouter.js
@@ -3,8 +3,14 @@ import { Router } from 'express';
 const appRouter = Router();
 
 appRouter.get('/', (req, res) => {
+  const links = [
+    { href: '/', text: 'Home' },
+    { href: 'about', text: 'About' },
+  ];
+
   res.render('index', {
     message: 'This is a message from appRouter to index view.',
+    links: links,
   });
 });
 
