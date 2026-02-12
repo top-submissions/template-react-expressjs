ad262cd42353a3b02d28d455874c325026f37dce
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-02-12 21:02:36 +0800

    feat(app): setup views
    What:
    - Import node:path and fileURLToPath
    
    Why:
    - fileURLToPath was imported since ES modules doesn't have ___dirname (need to declare)

--- server/app.js
+++ server/app.js
@@ -1,12 +1,24 @@
 import { config } from 'dotenv';
 import express from 'express';
 import appRouter from './routes/appRouter.js';
+import * as path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 config();
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// View setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Routes
 app.use('/', appRouter);
+
+// Error handling
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(err.statusCode || 500).send(err.message);
