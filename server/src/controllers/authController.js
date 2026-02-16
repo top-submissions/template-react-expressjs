0af69cb6fd7a04be1fba1cadd8a68043f17803d6
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-02-16 15:09:17 +0800

    feat(router, controller): add get and post for login
    fix: missing imports for pool and passport in controller

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,13 +1,24 @@
+import { MyPool } from '../db/pool';
+import passport from 'passport';
+
 export const authSignupGet = (req, res) => res.render('auth/sign-up-form');
 
 export const authSignupPost = async (req, res, next) => {
   try {
-    await pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [
-      req.body.username,
-      req.body.password,
-    ]);
+    await MyPool.query(
+      'INSERT INTO users (username, password) VALUES ($1, $2)',
+      [req.body.username, req.body.password],
+    );
     res.redirect('/');
   } catch (err) {
     return next(err);
   }
 };
+
+export const authLoginGet = (req, res) =>
+  res.render('index', { user: req.user });
+
+export const authLoginPost = passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/',
+});
--- server/src/routes/authRouter.js
+++ server/src/routes/authRouter.js
@@ -3,7 +3,12 @@ import * as authController from '../controllers/authController.js';
 
 const authRouter = Router();
 
+// Signup
 authRouter.get('/sign-up', authController.authSignupGet);
 authRouter.post('/sign-up', authController.authSignupPost);
+
+// Login
+authRouter.get('/', authController.authLoginGet);
+authRouter.post('/log-in', authController.authLoginPost);
 
 export default authRouter;
