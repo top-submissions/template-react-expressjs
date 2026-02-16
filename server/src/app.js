f1b874765bdc2ecbc6b3639831b68650022e8abc
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-02-16 15:21:50 +0800

    feat(app): store currentUser in session during setup

--- server/src/app.js
+++ server/src/app.js
@@ -32,6 +32,10 @@ app.use(
   }),
 );
 app.use(passport.session());
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 // Body parsing
 app.use(express.urlencoded({ extended: true }));
