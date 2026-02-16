c319149459a41bd2e5771530749e0ba2ba2540c5
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-02-16 14:55:01 +0800

    feat(router): add post route for signup

--- server/src/routes/authRouter.js
+++ server/src/routes/authRouter.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import * as authController from '../controllers/authController.js';
 
 const authRouter = Router();
 
-authRouter.get('/sign-up', (req, res) => res.render('auth/sign-up-form'));
+authRouter.get('/sign-up', authController.authSignupGet);
+authRouter.post('/sign-up', authController.authSignupPost);
 
 export default authRouter;
