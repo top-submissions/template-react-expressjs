37f2d9c4b5bf8c66ef90c211eba3aaf1990ffd47
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-02-17 08:44:43 +0800

    feat(router): add route for index
    fix: login GET directing to index

--- server/src/routes/authRouter.js
+++ server/src/routes/authRouter.js
@@ -3,12 +3,15 @@ import * as authController from '../controllers/authController.js';
 
 const authRouter = Router();
 
+// Home
+authRouter.get('/', authController.authIndexGet);
+
 // Signup
 authRouter.get('/sign-up', authController.authSignupGet);
 authRouter.post('/sign-up', authController.authSignupPost);
 
 // Login
-authRouter.get('/', authController.authLoginGet);
+authRouter.get('/log-in', authController.authLoginGet);
 authRouter.post('/log-in', authController.authLoginPost);
 
 // Logout
