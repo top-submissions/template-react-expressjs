821e5f6216fb8c3cb25b0f7654435f278ff2a8fa
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-03-14 09:02:43 +0800

    build(config): add vitest, add localStorage mock

--- client/src/__tests__/setup.js
+++ client/src/__tests__/setup.js
@@ -7,3 +7,36 @@ expect.extend(matchers);
 afterEach(() => {
   cleanup();
 });
+
+/**
+ * Creates a mock implementation of localStorage for testing
+ * @returns {Object} Mock localStorage with all standard methods
+ */
+const createLocalStorageMock = () => {
+  let store = {};
+
+  return {
+    getItem: (key) => store[key] || null,
+    setItem: (key, value) => {
+      store[key] = String(value);
+    },
+    removeItem: (key) => {
+      delete store[key];
+    },
+    clear: () => {
+      store = {};
+    },
+    key: (index) => Object.keys(store)[index] || null,
+    get length() {
+      return Object.keys(store).length;
+    },
+  };
+};
+
+if (typeof window !== 'undefined') {
+  Object.defineProperty(window, 'localStorage', {
+    value: createLocalStorageMock(),
+    writable: true,
+    configurable: true,
+  });
+}
